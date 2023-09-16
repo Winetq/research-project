c981faa98625bf51e7da7419dc31969d8bab7d89
Winetq <winetq9679@gmail.com>
2023-09-16 11:29:05 +0200

    Track prepared sql statements v1

--- instrumentation/src/main/java/agent/Agent.java
+++ instrumentation/src/main/java/agent/Agent.java
@@ -4,48 +4,9 @@ import java.lang.instrument.Instrumentation;
 
 public class Agent {
 
-    public static void premain(String agentArgs, Instrumentation inst) {
+    public static void premain(String args, Instrumentation instr) {
         System.err.println("[Agent] START");
-        String className = "org.postgresql.jdbc.PgConnection";
-        transformClass(className, inst);
-    }
-
-    private static void transformClass(String className, Instrumentation instrumentation) {
-        Class<?> targetClass;
-        ClassLoader targetClassLoader;
-
-        // see if we can get the class using forName
-        try {
-            targetClass = Class.forName(className);
-            targetClassLoader = targetClass.getClassLoader();
-            transform(targetClass, targetClassLoader, instrumentation);
-            return;
-        } catch (Exception ex) {
-            System.err.println("Class [" + className + "] not found with forName!");
-        }
-
-        // otherwise iterate all loaded classes and find what we want
-        for (Class<?> clazz : instrumentation.getAllLoadedClasses()) {
-            if (clazz.getName().equals(className)) {
-                targetClass = clazz;
-                targetClassLoader = targetClass.getClassLoader();
-                transform(targetClass, targetClassLoader, instrumentation);
-                return;
-            }
-        }
-
-        throw new RuntimeException("Failed to find class [" + className + "]");
-    }
-
-    private static void transform(Class<?> clazz, ClassLoader classLoader, Instrumentation instrumentation) {
-        Transformer dt = new Transformer(clazz.getName(), classLoader);
-        instrumentation.addTransformer(dt, true);
-
-        try {
-            instrumentation.retransformClasses(clazz);
-        } catch (Exception ex) {
-            throw new RuntimeException("Transform failed for: [" + clazz.getName() + "]", ex);
-        }
+        instr.addTransformer(new Transformer("org.postgresql.jdbc.PgConnection", "prepareStatement"));
     }
 
 }
--- instrumentation/src/main/java/agent/Transformer.java
+++ instrumentation/src/main/java/agent/Transformer.java
@@ -2,49 +2,66 @@ package agent;
 
 import javassist.ClassPool;
 import javassist.CtClass;
+import javassist.CtField;
 import javassist.CtMethod;
 
+import java.io.ByteArrayInputStream;
 import java.lang.instrument.ClassFileTransformer;
 import java.security.ProtectionDomain;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Transformer implements ClassFileTransformer {
-
     private final String targetClassName;
+    private final String targetMethodName;
 
-    private final ClassLoader targetClassLoader;
+    private final Set<String> qqq = new HashSet<>();
 
-    public Transformer(String targetClassName, ClassLoader targetClassLoader) {
-        this.targetClassName = targetClassName;
-        this.targetClassLoader = targetClassLoader;
+    public Transformer(String targetClassName, String targetMethodName) {
+        this.targetClassName = targetClassName.replaceAll("\\.", "/");
+        this.targetMethodName = targetMethodName;
     }
 
     @Override
-    public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) {
-        // System.out.println(className); // shows lots of classes
-
+    public byte[] transform(ClassLoader loader, String className, Class classBeingRedefined,
+                            ProtectionDomain protectionDomain, byte[] classfileBuffer) {
         byte[] byteCode = classfileBuffer;
 
-        String finalTargetClassName = this.targetClassName.replaceAll("\\.", "/");
-
-        if (!className.equals(finalTargetClassName)) {
-            return byteCode;
-        }
-
-        if (loader.equals(targetClassLoader)) {
+        if (className.equals(targetClassName)) {
             System.err.println("[Agent] Transforming");
             try {
-                ClassPool cp = ClassPool.getDefault();
-                CtClass cc = cp.get(targetClassName);
-                CtMethod m = cc.getDeclaredMethod("prepareStatement");
+                ClassPool classPool = ClassPool.getDefault();
+                CtClass ctClass = classPool.makeClass(new ByteArrayInputStream(classfileBuffer));
 
-                // m.insertBefore("System.out.println(\"[Agent] I'm here!\");");
+                CtClass arrListClazz = classPool.get("java.util.ArrayList");
+                CtField ctField = new CtField(arrListClazz, "preparedSqlStatements", ctClass);
+                ctClass.addField(ctField);
 
-                m.insertBefore("agent.Debug.whoAmI();");
+                CtField f = new CtField(CtClass.booleanType, "initialized", ctClass);
+                ctClass.addField(f, "false");
 
-                byteCode = cc.toBytecode();
-                cc.detach();
+                classPool.importPackage("java.util");
+
+                CtMethod ctMethod = ctClass.getDeclaredMethod(targetMethodName);
+
+                ctMethod.insertBefore(
+                        "{" +
+                        "if (!initialized) {" +
+                        "preparedSqlStatements = new ArrayList();" +
+                        "initialized = true;" +
+                        "}" +
+                        "preparedSqlStatements.add($1);" +
+                        "System.err.println(preparedSqlStatements); " +
+                        "}"
+                );
+
+                //  kurr generyki nie sa wspierane XDDD, wszystko jasne
+
+                ctClass.writeFile();
+                byteCode = ctClass.toBytecode();
+                ctClass.detach();
             } catch (Exception e) {
-                System.err.println("[Agent] Exception - Transforming");
+                System.err.println(e.getMessage());
             }
         }
 
