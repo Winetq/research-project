b0eafe9319ea700269930ede33c642a606d2c94e
Michal Cwynar <michalcwynar@carfax.com>
2023-10-16 09:55:54 +0200

    Replace parameters with wildcards for VALUES

--- instrumentation/src/main/java/agent/DataStore.java
+++ instrumentation/src/main/java/agent/DataStore.java
@@ -9,10 +9,35 @@ public class DataStore {
     private final static Map<String, List<Long>> queryToTimeElapsed = new HashMap<>();
 
     public static void put(String query, long timeElapsed) {
-        List<Long> timeElapsedList = queryToTimeElapsed.computeIfAbsent(query, k -> new ArrayList<>());
+        String queryWithWildcards =  replaceParametersWithWildcards(query);
+        List<Long> timeElapsedList = queryToTimeElapsed.computeIfAbsent(queryWithWildcards, k -> new ArrayList<>());
         timeElapsedList.add(timeElapsed);
-        queryToTimeElapsed.put(query, timeElapsedList);
+        queryToTimeElapsed.put(queryWithWildcards, timeElapsedList);
         System.err.println(queryToTimeElapsed);
-        System.err.println(query + " executed in " + timeElapsed + " microseconds");
+        System.err.println(queryWithWildcards + " executed in " + timeElapsed + " microseconds");
+    }
+
+    public static String replaceParametersWithWildcards(String sql) {
+        String valuesSqlKeyword = "VALUES";
+        if (sql.toUpperCase().contains(valuesSqlKeyword)) {
+            int indexOfValues = sql.indexOf(valuesSqlKeyword);
+            String sqlWithoutParameters = sql.substring(0, indexOfValues + valuesSqlKeyword.length());
+            long numberOfParameters = sqlWithoutParameters.chars().filter(ch -> ch == ',').count() + 1;
+            String wildcards = buildWildcards(numberOfParameters);
+            return sqlWithoutParameters.concat(wildcards);
+        }
+        return sql;
+    }
+
+    private static String buildWildcards(long numberOfParameters) {
+        StringBuilder sb = new StringBuilder("(");
+        for (int i = 1; i <= numberOfParameters; i++) {
+            if (i != numberOfParameters) {
+                sb.append("?, ");
+            } else {
+                sb.append("?)");
+            }
+        }
+        return sb.toString();
     }
 }
