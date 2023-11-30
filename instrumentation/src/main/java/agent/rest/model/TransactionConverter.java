ff5d16dde0b622b46da7e905d60c2c88fbf4cadc
Winetq <winetq9679@gmail.com>
2023-11-30 22:16:32 +0100

    Small refactoring: spaces, final, new lines

--- instrumentation/src/main/java/agent/rest/model/TransactionConverter.java
+++ instrumentation/src/main/java/agent/rest/model/TransactionConverter.java
@@ -6,10 +6,10 @@ import java.util.Map;
 
 public class TransactionConverter {
 
-    public List<Transaction> toTransaction(Map<String, List<Long>> queryToTime){
+    public List<Transaction> toTransaction(Map<String, List<Long>> queryToTime) {
         List<Transaction> transactionList = new ArrayList<>();
 
-        for (Map.Entry<String, List<Long>> entry: queryToTime.entrySet()) {
+        for (Map.Entry<String, List<Long>> entry : queryToTime.entrySet()) {
             transactionList.add(new Transaction(entry.getKey(), entry.getValue()));
         }
         return transactionList;
